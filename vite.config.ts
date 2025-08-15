b7197b6b89e66a503ca52774020bfe11aee64e10
Matt <designs.sleephawk@gmail.com>
2025-08-15 09:37:41 +0100

    updated base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/lyrics-full-stack/",
+  base: "/lyrics-front-end/",
   build: {
     rollupOptions: {
       input: {
